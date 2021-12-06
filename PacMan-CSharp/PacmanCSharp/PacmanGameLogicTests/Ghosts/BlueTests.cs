35bf12e8041c617bc3c9820b493617ccf927f6e7
ROHAN NIRER <R.NIRER-2021@hull.ac.uk>
2021-12-06 23:59:33 +0000

    added the mock repository and renamed and added the comments

--- PacMan-CSharp/PacmanCSharp/PacmanGameLogicTests/Ghosts/BlueTests.cs
+++ PacMan-CSharp/PacmanCSharp/PacmanGameLogicTests/Ghosts/BlueTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using Pacman.GameLogic.Ghosts;
 using System;
 using System.Collections.Generic;
@@ -12,199 +13,255 @@ namespace Pacman.GameLogic.Ghosts.Tests
     [TestClass()]
     public class BlueTests
     {
-        GameState gameState;
-        Ghost ghosts;
+        GameState _gameState;
+        Ghost _ghosts;
         Blue _blue;
         Pacman _pacman;
         Red _red;
 
+        private MockRepository mockRepository;
+        private Mock<GameState> mockGameState;
+        private Mock<Red> mockRed;
+        private Mock<Entity> mockEntity;
+
         [TestInitialize]
-        public void Initialize_Blue()
+        public void Initialize_Blue() // initialization and statement coverage
         {
-            gameState = new GameState();
-            _blue = new Blue(90, 110, gameState, 1.8f, 1.5f);
-            _pacman = new Pacman(Pacman.StartX, Pacman.StartY, gameState, 3.0f);
-            _red = new Red(90, 110, gameState, 1.8f, 1.9f);
+            this.mockRepository = new MockRepository(MockBehavior.Strict);
+            this.mockGameState = mockRepository.Create<GameState>(100);
+            this.mockEntity = mockRepository.Create<Entity>();
+            this.mockRed = mockRepository.Create<Red>(10, 10, this.mockGameState.Object, 1.8f, 1.9f);
+            _gameState = new GameState();
+            _blue = new Blue(90, 110, _gameState, 1.8f, 1.5f);
+            _pacman = new Pacman(Pacman.StartX, Pacman.StartY, _gameState, 3.0f);
+            _red = new Red(90, 110, _gameState, 1.8f, 1.9f);
         }
 
         [TestMethod()]
-        public void BlueTest()
+        public void BlueTest() // statement coverage
         {
-            ghosts = new Blue(Blue.StartX, Blue.StartY, new GameState(), 2.8f, 1.5f);
-            Assert.IsNotNull(ghosts);
+            _ghosts = new Blue(Blue.StartX, Blue.StartY, new GameState(), 2.8f, 1.5f);
+            Assert.IsNotNull(_ghosts);
+            this.mockRepository.VerifyAll();
         }
 
         [TestMethod()]
-        public void PacmanDeadTest()
+        public void PacmanDeadTest() // statement coverage
         {
             _blue.PacmanDead();
             Assert.AreEqual(95, Blue.StartX);
             Assert.AreEqual(118, Blue.StartY);
+            this.mockRepository.VerifyAll();
         }
 
         [TestMethod()]
-        public void ResetPositionTest()
+        public void ResetPositionTest() // statement coverage
         {
             _blue.ResetPosition();
             Assert.AreEqual(95, Blue.StartX);
             Assert.AreEqual(118, Blue.StartY);
+            this.mockRepository.VerifyAll();
         }
 
         [TestMethod()]
-        public void MoveFirstIfCaseTest()
+        public void Move_FirstIfCase_Test() // statement, edge, condition coverage 
         {
             _pacman.SetPosition(10, 10);
-            gameState.Pacman = _pacman;
+            _gameState.Pacman = _pacman;
             for (int i =0; i<100; i++)
                 _blue.Move();
-            Assert.IsTrue(true);
+            this.mockRepository.VerifyAll();
         }
 
         [TestMethod()]
-        public void MoveSecondIfCaseTest() //Distance(GameState.Red) < 50.0f
+        public void Move_SecondIfCase_Test() // statement, edge, condition coverage //Distance(GameState.Red) < 50.0f
         {
             _red.SetPosition(90, 110);
-            gameState.Red = _red;
+            _gameState.Red = _red;
             _pacman.SetPosition(90, 110);
-            gameState.Pacman = _pacman;
+            _gameState.Pacman = _pacman;
             _blue.Move();
-            Assert.IsTrue(true);
+            this.mockRepository.VerifyAll();
         }
 
         [TestMethod()]
-        public void MoveElseCaseTest() // Distance(GameState.Red) < 50.0f Else part
+        public void Move_ElseCase_Test() // statement, edge, condition coverage // Distance(GameState.Red) < 50.0f Else part
         {
             _red.SetPosition(10, 10);
-            gameState.Red = _red;
+            _gameState.Red = _red;
             _pacman.SetPosition(90, 110);
-            gameState.Pacman = _pacman;
+            _gameState.Pacman = _pacman;
             _blue.Move();
-            Assert.IsTrue(true);
+            this.mockRepository.VerifyAll();
         }
 
         [TestMethod()]
-        public void MoveIfElseCaseTest() 
+        public void Move_IfElseCase_Test() // statement, edge, condition coverage
         {
             _red.SetPosition(12, 10);
-            gameState.Red = _red;
+            _gameState.Red = _red;
             _pacman.SetPosition(90, 110);
-            gameState.Pacman = _pacman;
+            _gameState.Pacman = _pacman;
             _blue.Move();
-            Assert.IsTrue(true);
+            this.mockRepository.VerifyAll();
         }
 
         [TestMethod()]
-        public void MovePackManUpTest() // if( IsAbove(GameState.Pacman) )
+        public void Move_PackManUp_Test()// statement, edge, condition coverage  //true => if( IsAbove(GameState.Pacman))
         {
             _red.SetPosition(10, 10);
-            gameState.Red = _red;
+            _gameState.Red = _red;
             _pacman.SetPosition(90, 110);
             _pacman.SetDirection(Direction.Up);
-            gameState.Pacman = _pacman;
+            _gameState.Pacman = _pacman;
             _blue.Move();
-            Assert.IsTrue(true);
+            this.mockRepository.VerifyAll();
         }
 
         [TestMethod()]
-        public void MovePackManRightTest() // true => (x <= entity.x) for right => IsRight(GameState.Pacman)
+        public void Move_PackManRight_Test() // statement, edge, condition coverage // true => (x <= entity.x) for right => IsRight(GameState.Pacman)
         {
             _red.SetPosition(10, 10);
-            gameState.Red = _red;
+            _gameState.Red = _red;
             _pacman.SetPosition(190, 110);
-            gameState.Pacman = _pacman;
+            _gameState.Pacman = _pacman;
             _blue.Move();
-            Assert.IsTrue(true);
+            this.mockRepository.VerifyAll();
         }
 
         [TestMethod()]
-        public void MovePackManRightFalseTest() // false =>(x <= entity.x) for right => IsRight(GameState.Pacman)
+        public void Move_PackManRight_False_Test() // statement, edge, condition coverage // false =>(x <= entity.x) for right => IsRight(GameState.Pacman)
         {
             _red.SetPosition(10, 10);
-            gameState.Red = _red;
+            _gameState.Red = _red;
             _pacman.SetPosition(94, 110);
-            gameState.Pacman = _pacman;
+            _gameState.Pacman = _pacman;
             _blue.Move();
-            Assert.IsTrue(true);
+            this.mockRepository.VerifyAll();
         }
 
         [TestMethod()]
-        public void MovePackManDownTest() // Down //(Y <= entity.Y) IsAbove(GameState.Pacman) => false
+        public void Move_PackManDown_Test() // statement, edge, condition coverage // Down //(Y <= entity.Y) IsAbove(GameState.Pacman) => false
         {
             _red.SetPosition(10, 10);
-            gameState.Red = _red;
+            _gameState.Red = _red;
             _pacman.SetPosition(90, 120);
             _pacman.SetDirection(Direction.Up);
-            gameState.Pacman = _pacman;
+            _gameState.Pacman = _pacman;
             _blue.Move();
-            Assert.IsTrue(true);
+            this.mockRepository.VerifyAll();
         }
 
-        [TestMethod()]                        // true => IsBelow(GameState.Pacman)
-        public void MoveMinimizeXTrueTest() // false //Math.Abs(Node.X - GameState.Pacman.Node.X) != 0
+        [TestMethod()]                        // statement, edge, condition coverage 
+        public void Move_MinimizeX_True_Test() // true => IsBelow(GameState.Pacman) // false //Math.Abs(Node.X - GameState.Pacman.Node.X) != 0
         {
             _red.SetPosition(10, 10);
-            gameState.Red = _red;
+            _gameState.Red = _red;
             _pacman.SetPosition(97, 140);
             _pacman.SetDirection(Direction.Up);
-            gameState.Pacman = _pacman;
+            _gameState.Pacman = _pacman;
             _blue.Move();
-            Assert.IsTrue(true);
+            this.mockRepository.VerifyAll();
         }
 
-        [TestMethod()]                                  // true => IsBelow(GameState.Pacman) // true => IsLeft(GameState.Pacman)
-        public void MoveMinimizeXSecondCaseTrueTest() // false //Math.Abs(Node.X - GameState.Pacman.Node.X) != 0
+        [TestMethod()]                                  // statement, edge, condition coverage  // true => IsBelow(GameState.Pacman) // true => IsLeft(GameState.Pacman)
+        public void Move_MinimizeXSecondCase_True_Test() // false //Math.Abs(Node.X - GameState.Pacman.Node.X) != 0
         {
             _red.SetPosition(10, 10);
-            gameState.Red = _red;
+            _gameState.Red = _red;
             _pacman.SetPosition(95, 120);
             _pacman.SetDirection(Direction.Up);
-            gameState.Pacman = _pacman;
+            _gameState.Pacman = _pacman;
             for (int i = 0; i < 100; i++)
                 _blue.Move();
-            Assert.IsTrue(true);
+            this.mockRepository.VerifyAll();
         }
 
-        [TestMethod()]                                  // true => IsBelow(GameState.Pacman) // true => IsLeft(GameState.Pacman)
-        public void MoveMinimizeXSecondORCaseTrueTest() // false //Math.Abs(Node.X - GameState.Pacman.Node.X) != 0
+        [TestMethod()]                                 // statement, edge, condition coverage  // true => IsBelow(GameState.Pacman) // true => IsLeft(GameState.Pacman)
+        public void Move_MinimizeXSecond_ORCase_True_Test() // false //Math.Abs(Node.X - GameState.Pacman.Node.X) != 0
         {
             _red.SetPosition(12, 10);
-            gameState.Red = _red;
+            _gameState.Red = _red;
             _pacman.SetPosition(95, 120);
             _pacman.SetDirection(Direction.Up);
-            gameState.Pacman = _pacman;
+            _gameState.Pacman = _pacman;
             _blue.Move();
-            Assert.IsTrue(true);
+            this.mockRepository.VerifyAll();
         }
 
-        [TestMethod()]                        // false => IsBelow(GameState.Pacman)
-        public void MoveMinimizeXFalseTest() // false //Math.Abs(Node.X - GameState.Pacman.Node.X) != 0
+        [TestMethod()]                        // statement, edge, condition coverage  // false => IsBelow(GameState.Pacman)
+        public void Move_MinimizeX_False_Test() // false //Math.Abs(Node.X - GameState.Pacman.Node.X) != 0
         {
             _red.SetPosition(10, 10);
-            gameState.Red = _red;
+            _gameState.Red = _red;
             _pacman.SetPosition(97, 110);
             _pacman.SetDirection(Direction.Up);
-            gameState.Pacman = _pacman;
+            _gameState.Pacman = _pacman;
             _blue.Move();
-            Assert.IsTrue(true);
+            this.mockRepository.VerifyAll();
         }
 
-        [TestMethod()]                                  // true => IsBelow(GameState.Pacman) // true => IsLeft(GameState.Pacman)
-        public void MoveJustFindSomethingFirstElseIfTest() // false //Math.Abs(Node.X - GameState.Pacman.Node.X) != 0
+        [TestMethod()]                                  // statement, edge, condition coverage // true => IsBelow(GameState.Pacman) // true => IsLeft(GameState.Pacman)
+        public void Move_JustFindSomething_FirstElseIf_Test() // false //Math.Abs(Node.X - GameState.Pacman.Node.X) != 0
         {
-            gameState.Blue = _blue;
+            _gameState.Blue = _blue;
             _red.SetPosition(10, 10);
-            gameState.Red = _red;
+            _gameState.Red = _red;
             _pacman.SetPosition(95, 120);
             _pacman.SetDirection(Direction.Right);
-            gameState.Pacman = _pacman;
+            _gameState.Pacman = _pacman;
             _blue.Move();
-            Assert.IsTrue(true);
+            this.mockRepository.VerifyAll();
         }
 
         [TestMethod()]
-        public void CloneTest()
+        public void Move_Path_Coverage_1_Test() // path coverage
         {
-            Blue expected = new Blue(90, 110, gameState, 1.8f, 1.5f);
+            _red.SetPosition(90, 110);
+            _gameState.Red = _red;
+            _pacman.SetPosition(90, 110);
+            _gameState.Pacman = _pacman;
+            _blue.Move();
+            this.mockRepository.VerifyAll();
+        }
+
+        [TestMethod()]
+        public void Move_Path_Coverage_2_Test() // path coverage
+        {
+            _red.SetPosition(150, 100);
+            _gameState.Red = _red;
+            _pacman.SetPosition(190, 110);
+            _gameState.Pacman = _pacman;
+            _blue.Move();
+            this.mockRepository.VerifyAll();
+        }
+
+        [TestMethod()]
+        public void Move_Path_Coverage_3_Test() // path coverage
+        {
+            _red.SetPosition(150, 10);
+            _gameState.Red = _red;
+            _pacman.SetPosition(190, 10);
+            _gameState.Pacman = _pacman;
+            _blue.Move();
+            this.mockRepository.VerifyAll();
+        }
+
+        [TestMethod()]
+        public void Move_Path_Coverage_4_Test() // path coverage
+        {
+            _red.SetPosition(10, 10);
+            _gameState.Red = _red;
+            _pacman.SetPosition(90, 10);
+            _gameState.Pacman = _pacman;
+            _blue.Move();
+            this.mockRepository.VerifyAll();
+        }
+
+        [TestMethod()]
+        public void CloneTest() // statement coverage 
+        {
+            Blue expected = new Blue(90, 110, _gameState, 1.8f, 1.5f);
             expected.Clone();
             Blue parent = (Blue)((ICloneable)expected).Clone();
             Assert.AreEqual(expected.ImgX, _blue.ImgX);
